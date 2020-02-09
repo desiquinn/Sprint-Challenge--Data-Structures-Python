f23ed29972346e11674c5fd649a5bda3293a6187
Desiree Morris <desiquinncompany@gmail.com>
2020-02-09 16:15:53 -0500

    created a solution but has error with name not having a value

--- names/names.py
+++ names/names.py
@@ -1,4 +1,7 @@
 import time
+from binary_search_tree import BinarySearchTree as bst
+
+
 
 start_time = time.time()
 
@@ -11,10 +14,34 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
+
+"""
+# Orginal Solution
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+"""
+
+"""
+My Solution
+Time Complexity: 
+"""
+# Loop through first set of names 
+for name in names_1:
+    # add it to the binary search tree
+    bst.insert(name)
+
+# Loop through second set of names
+for name in names_2:
+    # if the bst contains the name
+    if bst.contains(name):
+        # add the name to duplicates list
+        duplicates.append(name)
+    # otherwise
+    else:
+        # add them to the binary search tree
+        bst.insert(name)
 
 
 end_time = time.time()
